b0c5e56087edda33bd8382c15281ce1b0018c4cb
TigranOg <tigagalaxy@gmail.com>
2013-09-14 18:31:12 +0400

    some changes

--- application/src/org/sosial/MainActivity.java
+++ application/src/org/sosial/MainActivity.java
@@ -1,15 +1,29 @@
 package org.sosial;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
 
-public class MainActivity extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
+
+import com.viewpagerindicator.PageIndicator;
+import com.viewpagerindicator.TitlePageIndicator;
+
+public class MainActivity extends FragmentActivity {
+    TestFragmentAdapter mAdapter;
+    ViewPager mPager;
+    PageIndicator mIndicator;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        mAdapter = new TestFragmentAdapter(getSupportFragmentManager());
+
+        mPager = (ViewPager)findViewById(R.id.pager);
+        mPager.setAdapter(mAdapter);
+
+        mIndicator = (TitlePageIndicator)findViewById(R.id.indicator);
+        mIndicator.setViewPager(mPager);
     }
 }
